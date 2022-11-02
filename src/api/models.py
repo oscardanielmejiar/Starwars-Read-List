0f6ba6b148ef263c334c1c9d91f83b379a945232
oscardanielmejiar <oscardanielmejiar@gmail.com>
2022-11-02 23:16:01 +0000

    need route

--- src/api/models.py
+++ src/api/models.py
@@ -5,11 +5,11 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(128), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User {}>'.format(self.email)
 
     def serialize(self):
         return {
@@ -73,6 +73,8 @@ class Vehicle(db.Model):
     passenger = db.Column(db.String(250))
     cost_in_credits = db.Column(db.String(250))
     vehicle_class = db.Column(db.String(250))
+    person_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+    person = db.relationship(Person)
 
     def serialize(self):
         return{
